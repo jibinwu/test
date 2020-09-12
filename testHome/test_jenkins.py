7168023ff15acd6734e3451ac5519ac26fc05b8e
jibinwu <295655698@qq.com>
2020-09-12 09:30:18 +0800

    update test_request.py

--- testHome/test_jenkins.py
+++ testHome/test_jenkins.py
@@ -1,4 +1,15 @@
-def add(a,b):
-    return a+b
-r=add(1,3)
-print(r)
+import requests
+import pytest
+import json
+def test_login():
+    r=requests.post(url='https://cms.mamaqunaer.com/user/login',
+                    data={"loginCode":"测试-季彬武","loginPassword":"123456a",},
+                    proxies={'https':'127.0.0.1:8888'},
+                    verify=False)
+
+    # print('status_code:'+str(r.status_code))
+    # print(r.url)
+    # print(r.headers)
+    print(type(r.json()))
+    print(r.headers['Set-Cookie'])
+    print(r.cookies)
--- testHome/test_requests.py
+++ testHome/test_requests.py
@@ -4,7 +4,7 @@ import json
 def test_login():
     r=requests.post(url='https://cms.mamaqunaer.com/user/login',
                     data={"loginCode":"测试-季彬武","loginPassword":"123456a",},
-                    proxies={'https':'127.0.0.1:8888'},
+                    # proxies={'https':'127.0.0.1:8888'},
                     verify=False)
 
     # print('status_code:'+str(r.status_code))
